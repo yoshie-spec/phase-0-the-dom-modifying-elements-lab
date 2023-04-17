573b8a177d23af392942a5ef2a37bc5413f8ff58
yosselin alvarez <yoshie_9311@icloud.com>
2023-04-16 20:22:16 -0400

    complete lap

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+const h1 = document.getElementById("victory");
+newHeader.innerHTML = "Yoshie is the champion";
